a405cafafe01ae377edc06359828e7c6813ecf38
Emmanuel Kipsang <emmqnuelkipsqng@gmail.com>
2023-04-30 12:22:05 -0700

    get recipe

--- src/App.js
+++ src/App.js
@@ -8,23 +8,40 @@ function App() {
   const APP_KEY = "d8b6e93ce7df2c4f7ff56206c1da858d"
 
   const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("chicken")
 
   useEffect(() => {
     getRecipes();
 
-  }, []);
+  }, [query]);
 
   const getRecipes = async () => {
-    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
     const data = await response.json();
     setRecipes(data.hits)
     console.log(data.hits)
   }
+
+  const updateSearch = (e) => {
+    setSearch(e.target.value)
+  }
+
+  const getSearch = (e) => {
+    e.preventDefault();
+    setQuery(search);
+  }
+
   return (
     <div className="App">
       <h1>Hello React</h1>
-      <form className="search-form">
-        <input className='search-bar' type="text" />
+      <form onSubmit={getSearch} className="search-form">
+        <input 
+        className='search-bar' 
+        type="text" 
+        value={search} 
+        onChange={updateSearch}
+        />
         <button className="search-button" type="submit">
           Search
         </button>
